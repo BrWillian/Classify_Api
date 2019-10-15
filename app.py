f39d6be782c24fbec2c6b008ce928dc3aa71e19c
brwillian <wiliam-m-@hotmail.com>
2019-10-15 17:25:01 -0300

    Correções

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, redirect
 import numpy as np
 import _pickle as pickle
 import cv2
 
+app = Flask(__name__)
+
 svm = pickle.load(open("svm_model.pkl", "rb"))
 
-app = Flask(__name__)
+ALLOWED_EXTENSIONS = ['jpg', 'jpeg', 'png', 'gif']
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
 
 @app.route('/')
 def hello_world():
@@ -17,17 +24,26 @@ def predict():
 
     requested_img = request.files['file']
 
-    image = np.asarray(bytearray(requested_img.read()), dtype="uint8")
-    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-    image = cv2.resize(image, (32, 32), interpolation=cv2.INTER_CUBIC)
-    image = image.flatten()
-    image = image / np.mean(image)
+    if requested_img and allowed_file(requested_img.filename):
 
-    x = image.reshape(1, -1)
+        image = np.asarray(bytearray(requested_img.read()), dtype="uint8")
+        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+        image = cv2.resize(image, (32, 32), interpolation=cv2.INTER_CUBIC)
+        image = image.flatten()
+        image = image / np.mean(image)
 
-    prediction = svm.predict(x)[0]
+        x = image.reshape(1, -1)
 
-    return jsonify({"Result": prediction})
+        prediction = svm.predict(x)[0]
+
+        return jsonify({"Result": prediction})
+
+    return redirect('/error')
+
+
+@app.route('/error')
+def error():
+    return render_template('error.html')
 
 
 
